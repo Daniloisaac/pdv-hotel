dd4449acdceb9aadbcbbc02f8eb25f95e569276a
danilo isaac <daniloisaac942@gmail.com>
2023-02-24 20:56:00 -0300

    refactor: ajustei nome na factory client

--- app/backend/src/infra/factories/ClientFactory.ts
+++ app/backend/src/infra/factories/ClientFactory.ts
@@ -4,8 +4,8 @@ import ClientUseCase from '../../domain/usecase/Client.usecase';
 import ClientController from '../controllers/Client.controller';
 import ClientAdapter from '../persistences/Client.persistence';
 
-const ipersitence: IClientPersistence<any> = new ClientAdapter()
-const clientRepository = new ClientRepository(ipersitence)
+const persistence: IClientPersistence<any> = new ClientAdapter()
+const clientRepository = new ClientRepository(persistence)
 const clientUseCase = new ClientUseCase(clientRepository)
 const controller = new ClientController(clientUseCase)
 
