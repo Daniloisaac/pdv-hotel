737ce94175d8a9f9bcf34c4a5c359a00e929db8d
danilo isaac <daniloisaac942@gmail.com>
2023-02-24 12:06:20 -0300

    feat: criei entidade client

---
+++ app/backend/src/domain/entities/client.ts
@@ -0,0 +1,13 @@
+export default class Client {
+  id: string;
+  name:string;
+  phone:string;
+  email: string
+
+  constructor() {
+    this.id = '0';
+    this.name = '';
+    this.phone = '';
+    this.email = '';
+  }
+}
